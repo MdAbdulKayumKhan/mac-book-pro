ba10b7cc18e5ca99b15aa0bd2408877f93ab3442
MdAbdulKayumKhan <kayum261@gmail.com>
2021-08-21 14:07:05 +0600

    button and promo working

--- js/app.js
+++ js/app.js
@@ -1,44 +1,118 @@
-function addCustom(product){
-    if(product == 'memory-cost-free'){
-        const price = 0;
-        return price;
-    } else{
-        const price = 180;
-        return price;
+function addCustom(product, price){
+    const productInput = product + '-cost';
+    // debugger;
+    // console.log(productInput);
+    
+    // memory costing
+    if(productInput == 'memory-free-cost'){
+        let memoryTotal = document.getElementById('memory-total');
+        memoryTotal.innerText = price;
+       
     }
+    else if(productInput == 'memory-180-cost'){
+        let memoryTotal = document.getElementById('memory-total');
+        memoryTotal.innerText = price;
+        
+    }
+    // ssd costing
+    else if(productInput == 'ssd-free-cost'){
+        let memoryTotal = document.getElementById('ssd-total');
+        memoryTotal.innerText = price;
+        
+    }
+    else if(productInput == 'ssd-100-cost'){
+        let memoryTotal = document.getElementById('ssd-total');
+        memoryTotal.innerText = price;
+        
+    }
+    else if(productInput == 'ssd-1tb-cost'){
+        let memoryTotal = document.getElementById('ssd-total');
+        memoryTotal.innerText = price;
+        
+    }
+
+    // delivery costing 
+    else if(productInput == 'delivery-free-cost'){
+        let memoryTotal = document.getElementById('delivery-total');
+        memoryTotal.innerText = price;
+        
+    }
+    else if(productInput == 'delivery-20-cost'){
+        let memoryTotal = document.getElementById('delivery-total');
+        memoryTotal.innerText = price;
+        
+    }
+    
+    return price;
     
 }
 
-function addSum(){
-    const memory = addCustom();
-    const total = memory + 1299;
-    // console.log(total);
+function addSum(x){
+    // debugger;
+    const totalOfferText = document.getElementById('total-offer');
+    const previousTotalText = document.getElementById('price-total');
+    const previousTotal = parseInt(previousTotalText.innerText);
+    // console.log(typeof x);
+    const total = x + previousTotal;
+    previousTotalText.innerText = total;
+    
+    totalOfferText.innerText = total;
     return total;
 }
 
+// Memory click buttons 
+document.getElementById('memory-free-cost').addEventListener('click', function(){
+  
+    const memoryPrice =  addCustom('memory-free', 0);
+    if(memoryPrice == 0){
+     const result = addSum(memoryPrice);
+    }
+});
 
-document.getElementById('memory-cost-free').addEventListener('click', function(){
-    const memoryPrice =  addCustom('memory-cost-free');
-    const memoryText = document.getElementById('memory-price');
-    memoryText.innerText = memoryPrice;
+document.getElementById('memory-180-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('memory-180', 180);
     
-});
-document.getElementById('memory-cost-16GB-180').addEventListener('click', function(){
-    const memoryPrice =  addCustom('memory-cost-16GB-180');
-    const memoryText = document.getElementById('memory-price');
-    memoryText.innerText = memoryPrice;
-    const totalSum = addSum();
-    const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = totalSum;
-    document.getElementById('total-offer').innerText = totalSum;
+        const result = addSum(memoryPrice);
+
 });
 
+// SSD click buttons
+document.getElementById('ssd-free-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('ssd-free', 0);
+    
+        const result = addSum(memoryPrice);
+});
+document.getElementById('ssd-100-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('ssd-100', 100);
+    
+        const result = addSum(memoryPrice);
+});
+document.getElementById('ssd-1tb-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('ssd-1tb', 180);
+    
+        const result = addSum(memoryPrice);
+});
+
+// delivery click buttons 
+document.getElementById('delivery-free-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('delivery-free', 0);
+    
+        const result = addSum(memoryPrice);
+});
+document.getElementById('delivery-20-cost').addEventListener('click', function(){
+    const memoryPrice =  addCustom('delivery-20', 20);
+    
+        const result = addSum(memoryPrice);
+});
+
+//  promo code apply
 document.getElementById('promo-button').addEventListener('click', function(){
+    // debugger;
     const promoInput = document.getElementById('promo-code');
     const promoCode = promoInput.value.toLowerCase();
     if(promoCode == 'stevekaku'){
-        const subTotal = addSum();
-        // console.log(discount);
+        const subTotal = addSum(0);
+        console.log(subTotal);
         const discount = subTotal * 0.2;
         const totalBill = subTotal - discount; 
         document.getElementById('total-offer').innerText =totalBill;
